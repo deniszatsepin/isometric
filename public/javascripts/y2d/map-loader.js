6002f4f639d5cb845d6145dbda91cfa36ac45080
Denis Zatsepin <denis@zatsepin.spb.ru>
2014-05-20 12:41:45 +0400

    Map preDraw

--- public/javascripts/y2d/map-loader.js
+++ public/javascripts/y2d/map-loader.js
@@ -1,4 +1,11 @@
 angular.module('Y2D')
+    .factory('Game', function(PIXI, async, _, Map) {
+        return {
+            stage: new PIXI.Stage(0xee0000),
+            renderer: PIXI.autoDetectRenderer(640, 480),
+            graphics: new PIXI.Graphics()
+        }
+    })
     .factory('Map', function(PIXI, async, _, Tileset) {
         function Map() {
             PIXI.EventTarget.call( this );
@@ -6,30 +13,86 @@ angular.module('Y2D')
         }
 
         Map.prototype.init = function(map) {
+            this.mapData = map;
             this.tilesets = [];
+            this.tiles = [];
             if (map.tilesets && map.tilesets.length) {
-                this.__loadTilesets(map.tilesets);
+                this.__loadTilesets();
             }
         };
 
-        Map.prototype.__loadTilesets = function(tilesets) {
-            var scope = this;
-            async.every(tilesets, function(tileset, done) {
+        Map.prototype.__loadTilesets = function() {
+            var tilesets = this.mapData.tilesets;
+            async.every(tilesets, _.bind(function(tileset, done) {
                 var tile = new Tileset(tileset);
+
                 tile.on('loaded', function() {
                     done(true);
                 });
                 tile.on('error', function(err) {
                     done(false);
                 });
-                scope.tilesets.push(tile);
-            }, _.bind(function(result) {
+                this.tilesets.push(tile);
+            }, this), _.bind(function(result) {
                 if (result) {
                     this.emit({type: 'loaded'});
                 } else {
                     this.emit({type: 'error'});
                 }
             }, this));
+        };
+
+        Map.prototype.getTileById = function(tileId) {
+            if (tileId < 1) return null;
+            var tilesets = this.tilesets;
+            var tileset = null;
+            for (var i = 0, len = tilesets.length; i < len; i += 1) {
+                if (tileId === tilesets[i].firstgid) {
+                    tileset = tilesets[i];
+                    break;
+                }
+
+                if (tileId < tilesets[i].firstgid) {
+                    tileset = tilesets[i - 1];
+                    break;
+                }
+            }
+
+            if (tileset === null) {
+                tileset = tilesets[len - 1];
+            }
+
+            //TODO: set x and y
+            var rectangle = new PIXI.Rectangle(x, y, tileset.tilewidth, tileset.tileheight);
+            return {
+                texture: new PIXI.Texture(tileset.texture, rectangle),
+                getSprite: function (x, y) {
+                    var sprite = new PIXI.Sprite(this.texture);
+                    sprite.position.x = x;
+                    sprite.position.y = y;
+
+                    sprite.anchor.x = 0;
+                    sprite.anchor.y = 1;
+                    return sprite;
+                }
+            };
+        },
+
+        Map.prototype.preDraw = function() {
+            var layers = this.mapData.layers;
+            _.each(layers, _.bind(function(layer, id) {
+                var data = layer.data;
+                for (var i = 0, len = data.length; i < len; i += 1) {
+                    var tileId = data[i];
+                    if (!this.tiles[tileId]) {
+                        this.tiles[tileId] = this.getTileById(tileId);
+                    }
+                }
+            }, this));
+        };
+
+        Map.prototype.draw = function() {
+
         };
 
         return Map;
